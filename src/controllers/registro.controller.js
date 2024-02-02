5e120172235af57afaffed977f9fd33cde81f576
Felipe Herrera <fherrera7850@gmail.com>
2024-02-02 03:31:37 -0300

    registro listo

--- src/controllers/registro.controller.js
+++ src/controllers/registro.controller.js
@@ -1,8 +1,8 @@
-import {getConnection} from ".././database/database";
+import { getConnection } from ".././database/database";
 //import bcrypt from 'bcrypt';
 
 const registrarUsuario = async (req, res) => {
-    const {idRol, nombreUsuario, passwordUsuario, email, direccion, telefono, activo, nombreCompleto} = req.body;
+    const { idRol, nombreUsuario, passwordUsuario, email, direccion, telefono, activo, nombreCompleto } = req.body;
 
     if (!nombreUsuario || !passwordUsuario || !email || !direccion || !telefono || !nombreCompleto) {
         res.status(400).json({ message: "Bad Request. Please fill all required fields." });
@@ -16,22 +16,23 @@ const registrarUsuario = async (req, res) => {
 
         //const hashedPassword = await bcrypt.hash(passwordUsuario, 10);
 
-        const usuarioNuevo = { 
+        const usuarioNuevo = {
             idRol,
-            nombreUsuario, 
+            nombreUsuario,
             passwordUsuario, //: hashedPassword, 
-            email, 
-            direccion, 
-            telefono, 
+            email,
+            direccion,
+            telefono,
             activo: 1,
             nombreCompleto,
         };
 
-        await connection.query("INSERT INTO usuario SET ?", usuarioNuevo);//req.body //('INSERT INTO usuarios (nombreUsuario, passwordUsuario, email, direccion, telefono, activo) VALUES (?, ?, ?, ?, ?, ?)', [nombreUsuario, hashedPassword, email, direccion, telefono, 1])
+        const result = await connection.query("INSERT INTO usuario SET ?", usuarioNuevo);//req.body //('INSERT INTO usuarios (nombreUsuario, passwordUsuario, email, direccion, telefono, activo) VALUES (?, ?, ?, ?, ?, ?)', [nombreUsuario, hashedPassword, email, direccion, telefono, 1])
         await connection.query("commit")
 
-        //el error 200 es una respuesta HTTP al cliente que significa que la solicitud fue exitosa
-        res.sendStatus(200);
+        //el codigo 200 es una respuesta HTTP al cliente que significa que la solicitud fue exitosa, tambien se retorna el id del  nuevo usuario
+        res.json(result.insertId);
+        res.status(200);
     } catch (error) {
         //rollback es la reversion de la query, es decir revierte o invalida la inserción de datos en caso de error
         await connection.query("rollback")
